fc2071824585ad6cd877018044a62cbda5f29898
nemakazuki <nemakazuki@open.ed.jp>
2025-07-08 18:17:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,8 @@ def handle_message(event):
         TextSendMessage(text=reply_text)
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
